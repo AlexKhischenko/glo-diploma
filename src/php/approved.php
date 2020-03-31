d728fae5265cde4cd7adde076f2e3ef686afc6f9
Alex Khischenko <Khischenko.A@gmail.com>
2020-03-31 14:52:08 +0300

    feat: added categories section

---
+++ src/php/approved.php
@@ -0,0 +1,46 @@
+<section class="section approved">
+      <div class="container">
+        <h2 class="approved__title"><span class="approved__title-word">УТВЕРЖДЕННАЯ ФОРМА</span> ДЛЯ ШКОЛ Г.НУРСУЛТАН<i class="fas fa-angle-right approved__title-icon"></i></h2>
+        <div class="approved__container product-section">
+          <div class="col-30 product-section__card">
+            <div class="product-section__image">
+              <img src="./img/approved/boy.png" alt="Boy">
+            </div>
+            <!-- /.product-section__image -->
+            <div class="product-section__data">
+              <span class="product-section__category">Популярные товары</span>
+              <p class="product-section__title">для мальчиков</p>
+              <a href="./catalog.php" class="btn product-section__btn">Подробнее<i class="fas fa-angle-right btn__icon"></i></a>
+            </div>
+            <!-- /.product-section__data -->
+          </div>
+          <div class="col-30 product-section__card">
+            <div class="product-section__image">
+              <img src="./img/approved/girl.png" alt="Boy">
+            </div>
+            <!-- /.product-section__image -->
+            <div class="product-section__data">
+              <span class="product-section__category">Популярные товары</span>
+              <p class="product-section__title">для девочек</p>
+              <a href="./catalog.php" class="btn product-section__btn">Подробнее<i class="fas fa-angle-right btn__icon"></i></a>
+            </div>
+            <!-- /.product-section__data -->
+          </div>
+          <div class="col-30 product-section__card">
+            <div class="product-section__image">
+              <img src="./img/approved/backpack.png" alt="Boy">
+            </div>
+            <!-- /.product-section__image -->
+            <div class="product-section__data">
+              <span class="product-section__category">Самые популярные</span>
+              <p class="product-section__title">аксессуары</p>
+              <a href="./catalog.php" class="btn product-section__btn">Подробнее<i class="fas fa-angle-right btn__icon"></i></a>
+            </div>
+            <!-- /.product-section__data -->
+          </div>
+        </div>
+        <!-- /.approved__container product-section -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <!-- /.section approved -->
---
+++ src/php/categories.php
@@ -0,0 +1,17 @@
+<div class="section categories">
+      <div class="container">
+        <div class="categories__wrapper">
+          <h2 class="categories__title section__title">Категории товаров</h2>
+          <div class="categories__variety">
+            <a href="./catalog.php" class="categories__item"><i class="categories__icon fas fa-table-tennis"></i>Развлечения, досуг</a>
+            <a href="./catalog.php" class="categories__item"><i class="categories__icon fas fa-wine-glass-alt"></i>Посуда и зеркала</a>
+            <a href="./catalog.php" class="categories__item"><i class="categories__icon fas fa-spray-can"></i>Парфюмерия и косметика</a>
+            <a href="./catalog.php" class="categories__item"><i class="categories__icon fas fa-keyboard"></i>Компьютерная техника</a>
+          </div>
+          <!-- /.categories__variety -->
+          <div class="categories__btn"><a href="./catalog.php" class="btn btn-more">Ещё<i class="btn-more__icon fas fa-angle-down"></i></a></div>
+        </div>
+      </div>
+      <!-- /.container -->
+    </div>
+    <!-- /.section categories -->
